7bb5ea7ad2f4fe69eb526650fcbcec88e81ea02d
YvanRandriamia <yvanrandriamia@gmail.com>
2025-11-29 14:43:04 +0300

    Modifier encore et encore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 app = FastAPI()
-data = {"id":1, "name":"rasoa", "email":"rasa@gmail.com"}
+data = [{"id":1, "name":"rasoa", "email":"rasa@gmail.com"}]
 @app.get("/")
 def getusers():
     return {"status":"success", "msg": "displayed successfully", "data": data}
